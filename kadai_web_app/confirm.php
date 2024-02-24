ff1c09d73e95f17252be1b1e383229e01354afa4
yuri19951117 <154530214+yuri19951117@users.noreply.github.com>
2024-02-25 01:23:57 +0900

    kadai_web_appを追加

---
+++ kadai_web_app/confirm.php
@@ -0,0 +1,45 @@
+<?php
+// POSTリクエストから入力データを取得
+$name = $_POST['user_name'];
+$age = $_POST['user_age'];
+$category = $_POST['category'];
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <h2>入力内容をご確認ください。</h2>
+    <p>問題なければ「確定」、修正する場合は「キャンセル」をクリックしてください。</p>
+
+    <table border="1">
+        <tr>
+            <th>項目</th>
+            <th>入力内容</th>
+        </tr>
+        <tr>
+            <td>社員名</td>
+            <td><?php echo $name; ?></td>
+        </tr>
+        <tr>
+            <td>年齢</td>
+            <td><?php echo $age; ?></td>
+        </tr>
+        <tr>
+            <td>所属部署</td>
+            <td><?php echo $category; ?></td>
+        </tr>
+    </table>
+
+    <p>
+        <button id="confirm-btn" onclick="location.href='complete.php';">確定</button>
+        <button id="cancel-btn" onclick="history.back();">キャンセル</button>
+    </p>
+</body>
+
+</html>
