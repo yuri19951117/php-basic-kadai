2acdc2e1aff90eed7c5ae2f58c63668eeddb39de
yuri19951117 <154530214+yuri19951117@users.noreply.github.com>
2024-02-26 23:39:02 +0900

    kadai_016を追加

--- kadai_016/class.php
+++ kadai_016/class.php
@@ -13,6 +13,10 @@
     private $name;
     private $price;
 
+public function show_price() {
+echo $this->price . '<br>';
+}
+
     public function __construct(string $name, int $price) {
     $this->name = $name;
     $this->price = $price;
@@ -24,6 +28,10 @@
     private $name;
     private $height;
     private $weight;
+
+    public function show_height() {
+        echo $this->height . '<br>';
+        }
 
     public function __construct(string $name, int $height, int $weight){
     $this->name = $name;
@@ -38,7 +46,12 @@
     $animal = new Animal('dog', 60, 5000);
 
     print_r($food);
+    echo '<br>';
     print_r($animal);
+    echo '<br>';
+
+    $food->show_price();
+    $animal->show_height();
 ?>
 
 </body>
